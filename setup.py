68e7ba3db32e1a8632cd317206769e5b5e96c9a8
Thibault Jouannic <thibault@miximum.fr>
2014-10-30 17:11:55 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-zipview',
-    version='0.1',
+    version='1.0.0',
     packages=['zipview'],
     include_package_data=True,
     license='MIT License',  # example license
